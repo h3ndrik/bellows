d281d6d9ace29f874de5646109ee177c552679a2
Yoda-x <Yoda-x>
2018-06-24 23:54:40 +0200

    debug route record

--- bellows/zigbee/application.py
+++ bellows/zigbee/application.py
@@ -232,9 +232,10 @@ class ControllerApplication(zigpy.application.ControllerApplication):
             LOGGER.warning("Unexpected message send notification")
         except asyncio.futures.InvalidStateError as exc:
             LOGGER.debug("Invalid state on future - probably duplicate response: %s", exc)
+
     def _handle_route_record(self, args):
         LOGGER.debug("Route Record:%s", args)
-        
+
     @zigpy.util.retryable_request
     async def request(self, nwk, profile, cluster, src_ep, dst_ep, sequence, data, expect_reply=True, timeout=10):
         assert sequence not in self._pending
