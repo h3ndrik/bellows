51ba23a6bf05a02701f1dda8f12c991987a357c7
Yoda-x <34125458+Yoda-x@users.noreply.github.com>
2018-10-28 23:39:50 +0100

    update version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="bellows",
-    version="0.7.3-Y-pre",
+    version="0.7.4-Y",
     description="Library implementing EZSP",
     url="http://github.com/Yoda-x/bellows",
     author="orig by Russell Cloran",
