e97aac68488454f23dc205f24e35d59a53859e66
Yoda-x <34125458+Yoda-x@users.noreply.github.com>
2018-11-30 12:17:43 +0100

    bump version to differentiate from upstream

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="bellows",
-    version="0.7.4.3.dev0",
+    version="100.7.4.3.dev0",
     description="Library implementing EZSP",
 #    url="http://github.com/Yoda-x/bellows",
     author="orig by Russell Cloran",
